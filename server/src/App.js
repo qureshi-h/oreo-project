491d06f52f422dba76fc670abcbb66b5facd1736
Hamza <hamza.ahmedqureshi@hotmail.com>
2022-02-02 12:12:33 +1100

    Added Landing Page

--- server/src/App.js
+++ server/src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { LandingPage } from "./Pages/LandingPage";
+
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<LandingPage />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
---
+++ server/src/Components/LandingPage/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <div className="background">
+            <div style={{ textAlign: "center", width: "100vw" }}>
+                <h1>The Oreo Project</h1>
+                <h2>Keeping you entertained since 1999</h2>
+            </div>
+        </div>
+    );
+};
