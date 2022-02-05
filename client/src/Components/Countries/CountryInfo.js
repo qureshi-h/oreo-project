2f0a5573b125a170aab2e2bbc64381c473d56ba1
Hamza <hamza.ahmedqureshi@hotmail.com>
2022-02-05 22:12:48 +1100

    added country info page

--- client/src/Components/Countries/CountryInfo.js
+++ client/src/Components/Countries/CountryInfo.js
@@ -1,14 +1,60 @@
 import React from "react";
 
+import { useParams } from "react-router-dom";
+import { CountryMap } from "./CountryMap";
+import InfoTable from "./InfoTable";
+
 export const CountryInfo = ({ country }) => {
+    const [info, setInfo] = React.useState(null);
+
+    React.useEffect(() => {
+        getInfo();
+    }, []);
+
     const getInfo = () => {
         fetch(`https://restcountries.com/v3.1/name/${country}`, {
             method: "GET",
         })
-            .then((response) => response.json)
+            .then((response) => response.json())
             .then((data) => {
-                console.log(data);
+                console.log(data[0]);
+                setInfo(data[0]);
             });
     };
-    return <div></div>;
+
+    return (
+        <div>
+            {info && (
+                <div className="countryInfo">
+                    <div
+                        style={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "right",
+                            marginRight: "10vw",
+                        }}
+                    >
+                        <h1>{info.name.official}</h1>
+                        <img
+                            src={info.flags.png}
+                            alt="flag"
+                            className="countryFlag"
+                        />
+                    </div>
+                    <div
+                        style={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "left",
+                        }}
+                    >
+                        <div className="infoTable">
+                            <InfoTable info={info} />
+                        </div>
+                        <CountryMap country={info.name.common} />
+                    </div>
+                </div>
+            )}
+        </div>
+    );
 };
--- client/src/Pages/CountryInfoPage.js
+++ client/src/Pages/CountryInfoPage.js
@@ -3,16 +3,18 @@ import { Helmet } from "react-helmet";
 import { NavigationBar } from "../Components/UIElements/NavigationBar";
 
 import { useParams } from "react-router-dom";
+import { CountryInfo } from "../Components/Countries/CountryInfo";
 
 export const CountryInfoPage = () => {
     const { country } = useParams();
     return (
-        <div className="countriesBackground">
+        <div>
             <Helmet>
                 <title>The Oreo Project | </title>
-                <style>{"body { background-color: #060026; }"}</style>
+                <style>{"body { background-color: #28004d; }"}</style>
             </Helmet>
             <NavigationBar opacity={0.4} />
+            <CountryInfo country={country} />
         </div>
     );
 };
